68ece4e226ede9104f2a0216fca2ff94d2cadaa9
jhvlima <joao.h.lima@edu.ufes.br>
2023-10-30 23:13:17 -0300

    adicao do makefile

--- Testes/cosseno.sh
+++ Testes/cosseno.sh
@@ -10,5 +10,5 @@ gcc -o cosseno_vetores cosseno_vetores.c -lm
 wc -l banco.txt > num_palavras.txt
 
 # chama o programa que calcula o coseno dos vetores passados como argumento e precisa da quantidade de palavras
-./cosseno_vetores num_palavras.txt vet/teste.vet vet/TESTE3.vet
+./cosseno_vetores num_palavras.txt vet/teste.vet vet/caminho1/teste2.vet
 
