ea9db0d487263067693e1188ea632480572d644e
jhvlima <joao.h.lima@edu.ufes.br>
2023-12-14 17:57:33 -0300

    conta palavras

--- libDocumentos.c
+++ libDocumentos.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SCRIPT_1 "find %s -type f | wc -l"
-#define SCRIPT_2 "find %s -type f -name %s | wc -l"
+#define SCRIPT_CONTA_DOCS_1 "find %s -type f | wc -l"
+#define SCRIPT_CONTA_DOCS_2 "find %s -type f -name %s | wc -l"
+
+#define SCRIPT_CONTA_PALAVRAS_1 "find %s type f -exec sh -c cat {} | wc -w"
+#define SCRIPT_CONTA_PALAVRAS_2 "cat $(find %s -type f) | wc -w"
 
 void ContaDocumentos(const char *caminho, const char *parametros)
 {
@@ -12,12 +15,36 @@ void ContaDocumentos(const char *caminho, const char *parametros)
     
     if (parametros == NULL)
     {
-        snprintf(comando, sizeof(comando), SCRIPT_1, caminho);
+        sprintf(comando, SCRIPT_CONTA_DOCS_1, caminho);
     }
     else
     {
-        snprintf(par, sizeof(par), "\"%s\"", parametros);
-        snprintf(comando, sizeof(comando), SCRIPT_2, caminho, par);
+        sprintf(par, "\"%s\"", parametros);
+        sprintf(comando, SCRIPT_CONTA_DOCS_2, caminho, par);
+    }
+
+    printf("Executando: %s\n", comando);
+    int resultado = system(comando);
+
+    if (resultado == -1)
+    {
+        perror("Erro ao executar o comando");
+    }
+}
+
+void ContaPalavras(const char *caminho, const char *parametros)
+{
+    // Construir o comando usando o caminho fornecido
+    char comando[1024];
+    char par[256];
+    
+    if (parametros == NULL)
+    {
+        sprintf(comando, SCRIPT_CONTA_PALAVRAS_2, caminho);
+    }
+    else
+    {
+        sprintf(comando, SCRIPT_CONTA_PALAVRAS_2, caminho, par);
     }
 
     printf("Executando: %s\n", comando);
@@ -40,15 +67,25 @@ int main(int argc, char *argv[])
 
     // Obter o caminho do argumento da linha de comando
     const char *path = argv[1];
-    const char *parametros = NULL;
+    const char *parametros = "";
 
     if (argc >= 2)
     {
         parametros = argv[2];
     }
 
-    // Chamar a função com o caminho fornecido
-    ContaDocumentos(path, parametros);
+    char opcao;
+    scanf("%c", &opcao);
+
+    switch (opcao)
+    {
+    case 'd':
+        ContaDocumentos(path, parametros);
+        break;
+    case 'p':
+        ContaPalavras(path, parametros);
+        break;
+    }
 
     return 0;
 }
